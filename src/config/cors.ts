b18b2308dc1f473e502c053eeca78a35182a02bc
hernanacevedo <42323463+hernanacevedo@users.noreply.github.com>
2024-12-06 15:54:34 -0300

    fix cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,17 @@
-import  {CorsOptions} from 'cors'
+import { CorsOptions } from 'cors';
 
-export const corsConfig: CorsOptions={
-    origin: function(origin,callback){
-        const whitelist=[process.env.FRONTEND_URL]
+export const corsConfig: CorsOptions = {
+  origin: (origin, callback) => {
+    const whitelist = [process.env.FRONTEND_URL];
 
-    if(process.argv[2]=== '--api'){
-        whitelist.push(undefined)
+    if (process.argv[2] === '--api') {
+      whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
     }
-    if(whitelist.includes(origin)){
-        callback(null,true)
+
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Permitir acceso
+    } else {
+      callback(new Error('Error de CORS: origen no permitido')); // Bloquear acceso
     }
-    else{
-        callback(new Error('Error de CORS'))
-    }
-    }
-}
+  },
+};
